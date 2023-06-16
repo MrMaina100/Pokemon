a5b70f3d1ada40885b2f3e83890ab6b1df4ff403
Collins Kiprono Kirui <101986591+iamkiprono@users.noreply.github.com>
2023-06-16 15:51:30 +0300

    Updates

--- src/Context/PokemonContext.jsx
+++ src/Context/PokemonContext.jsx
@@ -1,42 +1,29 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState } from "react";
 
 const PokemonContext = createContext();
 
-export const PokemonProvider = ({children})=>{
+export const PokemonProvider = ({ children }) => {
+  // change the default state an to array since the result from the api call will also be an array
+  const [items, setItems] = useState([]);
 
+  const fetchdata = async () => {
+    const res = await fetch("https://pokeapi.co/api/v2/pokemon");
+    const data = await res.json();
 
-   const [items, setItems] = useState({});
+    //  setitmes to the results array from the data result object
+    setItems(data.results);
+  };
 
+  return (
+    <PokemonContext.Provider
+      value={{
+        items,
+        fetchdata,
+      }}
+    >
+      {children}
+    </PokemonContext.Provider>
+  );
+};
 
-   useEffect(()=>{
-      setItems()
-
-   }, [])
-
-  
-
-   
-
-   const fetchdata = async ()=>{
-      const res = await fetch('https://pokeapi.co/api/v2/pokemon')
-      const data = await res.json();
-
-     
-
-      setItems(data)
-   }
-
-
-   return <PokemonContext.Provider value={{
-    items,
-    fetchdata
-
-   }}>
-
-      {children} 
-
-   </PokemonContext.Provider>
-
-}
-
-export default PokemonContext
+export default PokemonContext;
--- src/Pages/Pokemons.jsx
+++ src/Pages/Pokemons.jsx
@@ -1,26 +1,20 @@
-import PokemonContext from "../Context/PokemonContext"
-import { useContext } from "react"
+import PokemonContext from "../Context/PokemonContext";
+import { useContext, useEffect } from "react";
 function Pokemons() {
+  // destructuring the fetchdata function from the context so that we can call it from the pokemons component and update the items state on the context
+  const { items, fetchdata } = useContext(PokemonContext);
 
-  const {items} = useContext(PokemonContext)
+  // fecthing data on initial render of the component
+  useEffect(() => {
+    fetchdata();
+  }, []);
 
-  const {results} = items
-
- 
-  
-
-  
   return (
-    
     <div>
-
-      {results.map((result)=>{
-        return <li key={result.id}>{result.name}</li>
+      {items.map((result) => {
+        return <li key={result.id}>{result.name}</li>;
       })}
-      
-          
-      
     </div>
-  )
+  );
 }
-export default Pokemons
+export default Pokemons;
